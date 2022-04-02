e6d43bfed9742efea7ec3977d22f93e55e3160b2
cmahk <chandlermahkorn@gmail.com>
2022-04-02 10:54:05 -0700

    Python add/get user from DB

--- WorkoutBuddy.py
+++ WorkoutBuddy.py
@@ -2,4 +2,6 @@ from sql.db import Database
 
 db = Database()
 db.SetupSchema()
+db.AddUser("John", "Doe", "2/4/1920", "johndoe@gmail.com", "password1234")
+db.GetUser("John", "Doe")
 db.Close()
--- sql/db.py
+++ sql/db.py
@@ -18,3 +18,15 @@ class Database():
 
 	def Close(self):
 		self.db.close()
+
+	def GetUser(self, first, last):
+		self.cursor.execute("SELECT * FROM USER WHERE firstName LIKE ? AND lastName LIKE ?", (first, last))
+		print(self.cursor.fetchall())
+
+	def AddUser(self, first, last, dob, email, password):
+		self.cursor.execute("""INSERT INTO USER 
+								(firstName, lastName, dateOfBirth, email, password)
+								VALUES
+								(?, ?, ?, ?, ?)
+							""", (first, last, dob, email, password))
+		self.db.commit()
